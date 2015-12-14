a2db73a853a9a34bc663f4ee878664771448f303
Hasan Ahmed <ahmed.hasan09@gmail.com>
2015-12-14 16:01:01 +0000

    Create Histogram Project finish

---
+++ README.md
@@ -0,0 +1,5 @@
+# Histogram Project 
+
+## Codecademy Assignment
+
+Histogram is a program that takes a user's input , then builds a hash from that input. Each key in the hash is word from the usrer; each value is the number of times that word occurs. 
---
+++ histogram.rb
@@ -0,0 +1,8 @@
+puts "Enter some text:"
+text = gets.chomp
+words = text.split
+frequencies = Hash.new(0)
+words.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by { |key, value| value }
+frequencies.reverse!
+frequencies.each { |key, value| puts key + " " + value.to_s }
